fcb90ec5389fd5faf692f82020230b8d00dbbf94
aditeyapandey <aditeyapandey@gmail.com>
2022-04-13 11:01:10 -0400

    adding tree img

--- src/js/globalsettings.js
+++ src/js/globalsettings.js
@@ -24,5 +24,15 @@ var GLOBALDATA = {
       width:600,
       height:600
   },
-  currentFile:""
+  currentFile:"",
+  imgMap:{
+      tree:{
+          enclosure:{fileName:"ED.png",relativePath:"treeimg/"},
+          hybrid:{fileName:"HD.png",relativePath:"treeimg/"},
+          indented:{fileName:"IL.png",relativePath:"treeimg/"},
+          layered:{fileName:"LD.png",relativePath:"treeimg/"},
+          nodelink:{fileName:"NL.png",relativePath:"treeimg/"},
+          symbolic:{fileName:"SD.png",relativePath:"treeimg/"},
+      }
+  }
 };
