7a2868b275c7f5fa0879e8d54cea3af0ec42b324
aditeyapandey <aditeyapandey@gmail.com>
2022-04-16 15:15:12 -0400

    fixing the data read code

--- src/js/dataprocessing.js
+++ src/js/dataprocessing.js
@@ -25,38 +25,17 @@
     let fileName = window.GLOBALDATA.files[fileKey]["fileName"];
     let isHierarchy = window.GLOBALDATA.files[fileKey]["isHierarchy"];
     //If input data is already in hierarchy then parse as is
-    if (isHierarchy) {
-      d3.json("../assets/data/" + fileName).then((data) => {
+
+    d3.json("../assets/data/" + fileName).then((data) => {
+      if (isHierarchy) {
         window.GLOBALDATA.files[fileKey]["data"] = data;
         createDataObject(data);
         renderingControl.visUpdate();
-      });
-    } else {
-      //group by the hierarchy provided
-      let hierarchyOrder = window.GLOBALDATA.files[fileKey]["hierarchy"];
-      let values = window.GLOBALDATA.files[fileKey]["values"];
-      const mapToObject = (map = new Map()) =>
-        Array.from(map.entries(), ([k, v]) => {
-          if (v instanceof Map) {
-            return {
-              name: k,
-              children: v instanceof Map ? mapToObject(v) : v,
-            };
-          } else {
-            return {
-              name: k,
-              value: Object.keys(v["value"])
-                .map((val) => (v[val] = v["value"][val]))
-                .reduce((current, next) => {
-                  return { ...current, ...next };
-                }, {}),
-            };
-          }
-        });
-      // console.log(...hierarchy.map((a) => ))
-      d3.json("../assets/data/" + fileName).then((data) => {
+      } else {
+        let hierarchyOrder = window.GLOBALDATA.files[fileKey]["hierarchy"];
+        let values = window.GLOBALDATA.files[fileKey]["values"];
         var obj = {
-          name: "superstore",
+          name: "Superstore",
           children: mapToObject(
             d3.rollup(
               data,
@@ -78,9 +57,28 @@
         window.GLOBALDATA.files[fileKey]["data"] = obj;
         createDataObject(obj);
         renderingControl.visUpdate();
-      });
-    }
+      }
+    });
   };
+
+  const mapToObject = (map = new Map()) =>
+    Array.from(map.entries(), ([k, v]) => {
+      if (v instanceof Map) {
+        return {
+          name: k,
+          children: v instanceof Map ? mapToObject(v) : v,
+        };
+      } else {
+        return {
+          name: k,
+          value: Object.keys(v["value"])
+            .map((val) => (v[val] = v["value"][val]))
+            .reduce((current, next) => {
+              return { ...current, ...next };
+            }, {}),
+        };
+      }
+    });
 
   createDataObject = function (data) {
     //console.log(data);
@@ -91,32 +89,34 @@
 
     let maxDegree = -1;
     let maxDegreeNodeLabel = "";
-    root.each((d,i) =>{
-        if(d.children !==undefined)
-        {
-            if(d.children.length>maxDegree)
-            {
-                maxDegree = d.children.length
-                maxDegreeNodeLabel = d.data.name;
-            }
+    root.each((d, i) => {
+      if (d.children !== undefined) {
+        if (d.children.length > maxDegree) {
+          maxDegree = d.children.length;
+          maxDegreeNodeLabel = d.data.name;
         }
-    })
+      }
+    });
 
     let minDegree = 1000;
     let minDegreeNodeLabel = "";
-    root.each((d,i) =>{
-        if(d.children !==undefined)
-        {
-            if(d.children.length<minDegree)
-            {
-                minDegree = d.children.length;
-                minDegreeNodeLabel = d.data.name;
-            }
+    root.each((d, i) => {
+      if (d.children !== undefined) {
+        if (d.children.length < minDegree) {
+          minDegree = d.children.length;
+          minDegreeNodeLabel = d.data.name;
         }
-    })
+      }
+    });
 
-    var dataObj = new DataSummary(size,leafNodes,{maxDegree,maxDegreeNodeLabel},{minDegree,minDegreeNodeLabel},height,root)
+    var dataObj = new DataSummary(
+      size,
+      leafNodes,
+      { maxDegree, maxDegreeNodeLabel },
+      { minDegree, minDegreeNodeLabel },
+      height,
+      root
+    );
     window.GLOBALDATA.dataSummary = dataObj;
-
   };
 })();
