6bf67097f3eaffafdf9421c7979416d0b3f4401f
aditeyapandey <aditeyapandey@gmail.com>
2022-05-24 16:53:52 -0400

    adding descendants interaction to the nodelink

--- src/js/vis/dengrogram.js
+++ src/js/vis/dengrogram.js
@@ -132,24 +132,35 @@
         .on("mouseover", (e, d) => {
           //dendrogram.highlightNode("node_"+d.index, "select");
 
-          if (highlightAncestors) {
-            let ancestors = d.ancestors();
-            dendrogram.highlightAncestors(
-              "node_" + d.index,
-              ancestors,
-              "select"
-            );
+          // if (highlightAncestors) {
+          //   let ancestors = d.ancestors();
+          //   dendrogram.highlightAncestors(
+          //     "node_" + d.index,
+          //     ancestors,
+          //     "select"
+          //   );
+          // }
+          if(true)
+          {
+            let descendants = d.descendants();
+            dendrogram.highlightDescendants(descendants,"select");
           }
         })
         .on("mouseout", function (e, d) {
           //dendrogram.highlightNode("node_"+d.index, "deselect");
 
-          if (highlightAncestors) {
-            dendrogram.highlightAncestors(
-              "node_" + d.index,
-              [],
-              "deselect"
-            );
+          // if (highlightAncestors) {
+          //   dendrogram.highlightAncestors(
+          //     "node_" + d.index,
+          //     [],
+          //     "deselect"
+          //   );
+          // }
+
+          if(true)
+          {
+            let descendants = d.descendants();
+            dendrogram.highlightDescendants(descendants,"deselect");
           }
         });
 
@@ -228,6 +239,38 @@
           .transition()
           .duration("100")
           .style("opacity", "1");
+      }
+    } else {
+      d3.selectAll(".node").transition().duration("50").style("opacity", "1");
+      d3.selectAll(".link").transition().duration("50").style("opacity", "1");
+    }
+  };
+  dendrogram.highlightDescendants = function (descendants, event) {
+    if (event === "select") {
+      console.log(descendants);
+      d3.selectAll(".node").transition().duration("50").style("opacity", ".3");
+      d3.selectAll(".link").transition().duration("50").style("opacity", ".1");
+
+      // d3.select("#" + id)
+      //   .transition()
+      //   .duration("100")
+      //   .style("opacity", "1");
+      descendants.forEach((val) => {
+        d3.select("#node_" + val.index)
+          .transition()
+          .duration("100")
+          .style("opacity", "1");
+      });
+      for (var i = 0; i < descendants.length - 1; i++){
+        for(var j = 1; j< descendants.length; j++)
+        {
+          d3.select(
+            `#node_${descendants[i].index}-node_${descendants[j].index}`
+             )
+            .transition()
+            .duration("100")
+            .style("opacity", "1");
+        }
       }
     } else {
       d3.selectAll(".node").transition().duration("50").style("opacity", "1");
